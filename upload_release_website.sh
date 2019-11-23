de0b75703d10c0f0324072f29ae10b2f629a368e
Stefan Reuther <streu@gmx.de>
2019-11-23 16:06:51 +0100

    Add ability to build tagged website releases

---
+++ upload_release_website.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+#  Upload Website Release build
+#
+#  Host:
+#     bob dev --sandbox release/website-<date>
+#     upload_release_website.sh <date>
+#
+
+if test -z "$1"; then
+  echo "Please pass a release date as parameter"
+  exit 1
+fi
+
+p=$(bob query-path --release --sandbox "release::website-$1" -f {dist})
+if test -z "$p"; then
+  echo "Unable to find CI result"
+  exit 1
+fi
+
+rsync -vrlptS --delete "$p/" pcc:public_html/test/
